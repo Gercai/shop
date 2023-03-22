0ea2a7642c61b239ee32449abdbdf3b174feba06
Gerald <53179363+Gercai@users.noreply.github.com>
2023-03-22 10:34:12 +0000

    added shoppingcart basics

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import './shop.scss';
-import {Productdetails} from './modules/ProductDetails';
+import {Productdetails} from './modules/Productdetails';
 import {Footer} from './modules/Footer';
 import {Navbar} from './modules/Navbar';
 import {ProductCard} from './modules/ProductCard';
-import {Shoppingcart} from './modules/shopping';
+import {Shoppingcart} from './modules/Shoppingcart';
 
+import {useEffect,useState} from 'react';
 
 
 function App() {
+
+  // id, imageURL, category, title, description, size, color
+
+const [cartItems, setCartItems] = useState([{id: 1,
+    title: "greenShirt",
+  preis: 20},
+  {id: 2,
+    title: "greenShirt",
+  preis: 20}]);
+
+  console.log(cartItems);
+
+  // useEffect(()=> {
+  //   setCartItems(initialStateSC);
+  // },[])
+
   return (
     <div className="App">
       <Navbar></Navbar>
-      <Shoppingcart></Shoppingcart>
+      <Shoppingcart
+        cartItems = {cartItems}
+      />
       <ProductCard></ProductCard>
       <Productdetails></Productdetails>
       <Footer></Footer>
--- src/modules/Shoppingcart.jsx
+++ src/modules/Shoppingcart.jsx
@@ -1,5 +1,27 @@
-export const Shoppingcart = () => {
+import {useState, useEffect} from 'react';
+import '../css/shoppingcart.scss';
 
-    return (<div></div>)
-    
+export const Shoppingcart = ({cartItems}) => {
+
+const [gesamtPreis, setGesamtPreis] = useState();
+
+    const calculateCard = () => {
+       let total = cartItems.reduce((acc, cur) => {
+            acc = acc + cur.preis;
+        })
+        setGesamtPreis(total);
+    }
+
+return cartItems &&(
+    <div className="shoppingCard">
+        {cartItems.map((item,i) => (
+            <div>
+                <p>{item.preis}€</p>
+                <p>{item.title}</p>
+                
+        <button>+</button>
+        <button>-</button>
+            </div>))}
+            Total : {gesamtPreis}
+    </div>);   
 }
