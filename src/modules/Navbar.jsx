8f8deb4b1964cb98b510f9ea1120ce4592422351
vet <vetefir@gmail.com>
2023-03-22 12:59:27 +0100

    Link Bekledung - products

--- src/modules/Navbar.jsx
+++ src/modules/Navbar.jsx
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
+
 export const Navbar = () => {
 
 	return (
 
 		<nav className="footer">
 			<ul>
-				<li><a href="#"> Bekleidung</a></li>
+				<li><Link to={`/products/`}>Bekleidung</Link></li>
 				<li><a href="#"> Marken</a></li>
 				<li><a href="#"> Premium</a></li>
 				<li><a href="#"> Sale</a></li>
@@ -15,3 +17,6 @@ export const Navbar = () => {
 	)
     
 }
+
+
+
