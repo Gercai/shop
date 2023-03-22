91c1165ed813c044d1a30ee1b49c8d1b0048f27a
Bao Nguyen <Minzekatzebhn@gmail.com>
2023-03-22 10:26:03 +0100

    rename

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import './shop.scss';
-import {Productdetails} from './modules/ProductDetails';
-import {Footer} from './modules/Footer';
-import {Navbar} from './modules/Navbar';
-import {ProductCard} from './modules/ProductCard';
-import {Shoppingcart} from './modules/shopping';
-
-
+import "./shop.scss";
+import { ProductDetails } from "./modules/ProductDetails";
+import { Footer } from "./modules/Footer";
+import { Navbar } from "./modules/Navbar";
+import { ProductCard } from "./modules/ProductCard";
+import { ShoppingCart } from "./modules/ShoppingCart";
 
 function App() {
   return (
     <div className="App">
-      <Navbar></Navbar>
-      <Shoppingcart></Shoppingcart>
-      <ProductCard></ProductCard>
-      <Productdetails></Productdetails>
-      <Footer></Footer>
+      <Navbar />
+      <Shoppingcart />
+      <ProductCard />
+      <ProductDetails />
+      <Footer />
     </div>
   );
 }
--- src/modules/Shoppingcart.jsx
+++ src/modules/Shoppingcart.jsx
@@ -1,5 +1,3 @@
-export const Shoppingcart = () => {
-
-    return (<div></div>)
-    
-}
+export const ShoppingCart = () => {
+  return <div></div>;
+};
