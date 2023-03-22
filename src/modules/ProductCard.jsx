6b2bf984c20594e451e15061c0002ed642d37528
Bao Nguyen <Minzekatzebhn@gmail.com>
2023-03-22 15:15:12 +0100

    styling

--- src/modules/ProductCard.jsx
+++ src/modules/ProductCard.jsx
@@ -6,6 +6,8 @@ import { Button, CardActionArea, CardActions } from "@mui/material";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { BsEyeFill } from "react-icons/bs";
 
 export const ProductCard = () => {
   const [items, setItems] = useState([]);
@@ -28,7 +30,7 @@ export const ProductCard = () => {
             sx={{
               maxWidth: 300,
               margin: "0.75rem",
-              "&:hover": { boxShadow: "10px 10px 2px 1px #f7e5cb" },
+              "&:hover": { boxShadow: "8px 8px 2px 1px #d3d3d3" },
             }}
             className="itemCard"
           >
@@ -47,7 +49,7 @@ export const ProductCard = () => {
                 <CardContent>
                   <Typography
                     gutterBottom
-                    variant="h5"
+                    variant="h6"
                     component="div"
                     sx={{ color: "black" }}
                   >
@@ -67,21 +69,45 @@ export const ProductCard = () => {
               >
                 {item.price}€
               </Typography>
-              <Button
-                size="small"
-                sx={{
-                  color: "white",
-                  padding: "8px",
-                  fontSize: "bold",
-                  backgroundColor: "#e89637",
-                  "&:hover": {
-                    transform: "scale(1.05)",
+              <div>
+                <Link
+                  className="link"
+                  to={`/products/${item.id}`}
+                  style={{ textDecoration: "none" }}
+                >
+                  <Button
+                    size="small"
+                    sx={{
+                      color: "white",
+                      padding: "8px",
+                      marginRight: "0.25rem",
+                      fontSize: "bold",
+                      backgroundColor: "black",
+                      "&:hover": {
+                        transform: "scale(1.05)",
+                        backgroundColor: "black",
+                      },
+                    }}
+                  >
+                    <BsEyeFill />
+                  </Button>
+                </Link>
+                <Button
+                  size="small"
+                  sx={{
+                    color: "white",
+                    padding: "8px",
+                    fontSize: "bold",
                     backgroundColor: "#e89637",
-                  },
-                }}
-              >
-                Add
-              </Button>
+                    "&:hover": {
+                      transform: "scale(1.05)",
+                      backgroundColor: "#e89637",
+                    },
+                  }}
+                >
+                  <AiOutlineShoppingCart />
+                </Button>
+              </div>
             </CardActions>
           </Card>
         ))}
