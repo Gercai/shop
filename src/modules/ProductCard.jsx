f3753667569d5fe33bf775435a152d74c6aac9a5
Bao Nguyen <Minzekatzebhn@gmail.com>
2023-03-22 13:13:35 +0100

    adjusted

--- src/modules/ProductCard.jsx
+++ src/modules/ProductCard.jsx
@@ -21,64 +21,71 @@ export const ProductCard = () => {
   }, []);
 
   return (
-    <div>
-      {items.map((item) => (
-        <Card
-          sx={{
-            maxWidth: 300,
-            margin: "0.75rem",
-            "&:hover": { boxShadow: "10px 10px 2px 1px #f7e5cb" },
-          }}
-          className="itemCard"
-        >
-          <Link
-            className="link"
-            to={`/products/${item.id}`}
-            style={{ textDecoration: "none" }}
+    <div className="row d-flex justify-content-center">
+      <div className="col-sm-12 col-md-12 col-lg-10 col-xl-10 d-flex justify-content-center flex-wrap">
+        {items.map((item) => (
+          <Card
+            sx={{
+              maxWidth: 300,
+              margin: "0.75rem",
+              "&:hover": { boxShadow: "10px 10px 2px 1px #f7e5cb" },
+            }}
+            className="itemCard"
           >
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                height="300"
-                image={item.imageurl}
-                alt="green iguana"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  {item.title} {item.size}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  {item.description}
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Link>
-          <CardActions className="d-flex justify-content-between">
-            <Typography
-              variant="body2"
-              color="text.secondary"
-              sx={{ padding: "8px" }}
+            <Link
+              className="link"
+              to={`/products/${item.id}`}
+              style={{ textDecoration: "none" }}
             >
-              {item.price}€
-            </Typography>
-            <Button
-              size="small"
-              sx={{
-                color: "white",
-                padding: "8px",
-                fontSize: "bold",
-                backgroundColor: "#e89637",
-                "&:hover": {
-                  transform: "scale(1.05)",
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="300"
+                  image={item.imageurl}
+                  alt="green iguana"
+                />
+                <CardContent>
+                  <Typography
+                    gutterBottom
+                    variant="h5"
+                    component="div"
+                    sx={{ color: "black" }}
+                  >
+                    {item.title} {item.size}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {item.description}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Link>
+            <CardActions className="d-flex justify-content-between">
+              <Typography
+                variant="body2"
+                color="text.secondary"
+                sx={{ padding: "8px" }}
+              >
+                {item.price}€
+              </Typography>
+              <Button
+                size="small"
+                sx={{
+                  color: "white",
+                  padding: "8px",
+                  fontSize: "bold",
                   backgroundColor: "#e89637",
-                },
-              }}
-            >
-              Add
-            </Button>
-          </CardActions>
-        </Card>
-      ))}
+                  "&:hover": {
+                    transform: "scale(1.05)",
+                    backgroundColor: "#e89637",
+                  },
+                }}
+              >
+                Add
+              </Button>
+            </CardActions>
+          </Card>
+        ))}
+      </div>
     </div>
   );
 };
